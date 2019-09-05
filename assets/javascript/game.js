9e94dde3bfe74c5ce8594f93d626b0eb00a7a776
Zaya <zayazaw67@gmail.com>
2019-09-05 12:34:24 -0500

    got page to update wins. thanks paul

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,24 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
     "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+var wins = 0;
+var losses = 0;
+var guessedLetters = [];
 
-    
+var winsText = document.getElementById ("wins-text")
+var lossesText = document.getElementById ("losses-text")
+var guessesLeft = document.getElementById ("guessesLeft-text")
+var guessedLetters = document.getElementById ("guessedLetters-text")
+
 var computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log(computerSelection)
 
 
-document.onkeyup = function(event) {
-    var userGuess = event.key; 
-    console.log (userGuess);
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    console.log(userGuess);
     if (userGuess === computerSelection)
-    console.log("working")
+        console.log("working");
+        wins;
+        winsText.textContent = "Wins: " + wins;
 }
 
-// var winsText = document.getElementById (wins-text)
-// var lossesText = document.getElementById (losses-text)
-// var guessesLeft = document.getElementById (guessesLeft-text)
-// var guessedLetters = document.getElementById (guessedLetters-text)
