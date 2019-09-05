257546c7dcad89c8db892c3866f5f46602434983
Zaya <zayazaw67@gmail.com>
2019-09-05 11:43:00 -0500

    added computer choice, array, doc on key up

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+
+    
+var computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerSelection)
+
+
+document.onkeyup = function(event) {
+    var userGuess = event.key; 
+    console.log (userGuess);}
+
+// var winsText = document.getElementById (wins-text)
+// var lossesText = document.getElementById (losses-text)
+// var guessesLeft = document.getElementById (guessesLeft-text)
+// var guessedLetters = document.getElementById (guessedLetters-text)
