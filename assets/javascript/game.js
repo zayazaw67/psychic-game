01bcdf2775d4f84b74a631cfde99384ebf18eb3f
Zaya <zayazaw67@gmail.com>
2019-09-06 15:35:47 -0500

    added display for incorrect guesses, added alert when not entering a-z

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,51 +19,51 @@ document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
     console.log(userGuess);
     // how to make if user does not pick a-z???
-    // if (userGuess = [a-z]) {
-    //     console.log(userGuess)
-    // }
-    // else {alert("Please pick a letter")}
+    if (userGuess === [/a-zA-Z/]) {
+        console.log(userGuess)
+    }
+    else {alert("Please pick a letter");}
+
     if (userGuess === computerSelection) {
-        console.log("you win");
+        alert("you win");
         wins++;
         guessesLeft = 5;
         winsText.textContent = "Wins: " + wins;
         computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         compChoice = computerChoices[computerSelection];
-        console.log(computerSelection)
+        reset()
+        // console.log(computerSelection)
     }
     else {
-        console.log(guessesLeft);
+        // console.log(guessesLeft);
         guessesLeft --;
         guessesLeftText.textContent = ("Guesses Left: " + guessesLeft);
+        guessedLetters.push(userGuess);
+        display();
+        //push userguess to array
+        //call the function for letters
         
     }
     if (guessesLeft === 0) {
         losses ++;
-        alert("You Lost!");
         lossesText.textContent = "Losses: " + losses;
-        guessedLetters = [];
+        guessedLetters = "";
         guessedLettersText.textContent = guessedLetters;
+        alert("You Lost!");
         guessesLeft = 5;
+        guessesLeftText.textContent = ("Guesses Left: " + guessesLeft);
+        reset()
         computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         compChoice = computerChoices[computerSelection];
     }
 }
   
+function display() {
+    // display.push (guessedLettersText)
+    console.log(guessedLetters);
+    guessedLettersText.innerHTML = guessedLetters;
+}
 
-    
-
-
-// if (guessesLeft = 0)  {
-//     losses ++
-//     lossesText.textContent = "Losses: " + losses
-// }
-
-
-    //if statement when guesses =0 
-    // losses ++
-    // lossesText.textContent = "Losses: " + losses
-
-
-    // wins++
-    // winsText.textContent = "Wins: " + wins
+function reset() {
+    guessedLetters = []
+}
